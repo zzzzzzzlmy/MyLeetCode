3d91aed48129eabcc9b2cc2333a3ae1150d00cf8
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-09-30 11:26:07 +0800

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,26 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+'''Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+#Code is here
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in xrange(len(nums)):
+            x = nums[i]
+            if target-x in dict:
+                return (dict[target-x], i)
+            dict[x] = i
