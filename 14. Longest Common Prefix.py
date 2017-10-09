40339d3171ed5396a8595c6d861c7848d9558ba3
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-10-09 20:41:12 +0800

    Create 14. Longest Common Prefix.py

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,25 @@
+#Write a function to find the longest common prefix string amongst an array of strings.
+
+'''Example:
+Given strs = ['ab','abc','abuh','abcd','abcss','abg']
+return 'ab'
+'''
+
+#Code is here
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        
+        res = ''
+        for i in xrange(len(strs[0])):
+            for j in xrange(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
