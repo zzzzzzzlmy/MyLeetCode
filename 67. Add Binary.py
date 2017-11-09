16b0cfc7cc362acec7c6570ef1d814ec49dfeb65
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-11-09 20:50:09 +0800

    Update 67. Add Binary.py

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -9,6 +9,7 @@ Return "100".
 
 #Code is here
 
+#1
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -17,3 +18,26 @@ class Solution(object):
         :rtype: str
         """
         return bin(int(a, 2) + int(b, 2))[2:]
+    
+
+#2
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        if not a:
+            return b
+        if not b:
+            return a 
+        
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'       
+        elif a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '1'
+        
+        
