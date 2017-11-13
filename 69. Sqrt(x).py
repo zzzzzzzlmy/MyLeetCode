3be3401d4e026cdaa5b01bc57d4683e7d23510a2
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-11-13 21:08:53 +0800

    Create 69. Sqrt(x).py

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,35 @@
+'''
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+x is guaranteed to be a non-negative integer.
+'''
+
+#Example
+#Input: 4        Input: 8
+#Output: 2       Output: 2
+
+#Code is here
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0 or x == 1:
+            return x
+        i = 0
+        j = x
+        mid = 0
+        while True:
+            mid=(i + j) / 2
+            if mid > x / mid:
+                j = mid
+            else:
+                if(mid + 1) > x / (mid + 1):
+                    return mid
+                i = mid
+
+
