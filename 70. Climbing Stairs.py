4faa3a77fb3057b2577ed8ddb8160ac1e7c00f6e
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-11-15 20:36:54 +0800

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,20 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+'''
+
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+    
