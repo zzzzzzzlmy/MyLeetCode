16d9e33845971c7bb0ca8d10843c147e2e42a6b9
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-11-16 19:49:43 +0800

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -4,8 +4,7 @@ You are climbing a stair case. It takes n steps to reach to the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 Note: Given n will be a positive integer.
-'''
-'''
+
 Example 1:
 
 Input: 2
