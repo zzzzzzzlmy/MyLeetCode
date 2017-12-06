8927c274f5e0de818238df0222ce0aeffbe09e87
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-12-06 16:45:03 +0800

    Create 136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,16 @@
+'''
+Given an array of integers, every element appears twice except for one. Find that single one.
+'''
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
+        
+        
