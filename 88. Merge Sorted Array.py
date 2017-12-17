93518a921fd25a3b64713ccd08176f039e8a2400
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-12-17 13:22:27 +0800

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -1,7 +1,5 @@
 #Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-#Code is here
-
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
