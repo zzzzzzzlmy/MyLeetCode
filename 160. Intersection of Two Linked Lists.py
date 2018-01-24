22c726bcf147f47fe4e7ba14626421c9fd20c3f1
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2018-01-24 17:46:28 +0800

    Create 160. Intersection of Two Linked Lists.py

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,38 @@
+'''
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA is None or headB is None:
+            return None
+        
+        pa, pb= headA, headB
+        
+        while pa is not pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa
+        
+        
